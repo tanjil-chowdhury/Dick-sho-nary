5791fed4d4648aaceae44b3b2f42426a5dc43fa8
tanjil-chowdhury <tanjil.chowdhury000@gmail.com>
2025-09-09 02:59:05 +0600

    card

--- script.js
+++ script.js
@@ -8,6 +8,29 @@ const loadLessons = () => {
         .catch(error => console.error("Error fetching lessons:", error));
 }
 
+const loadLevelWords = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+   fetch(url)
+        .then(res => res.json())
+        .then(data => loadWord(data.data))
+        .catch(error => console.error("Error fetching words:", error));
+}
+
+const loadWord = (words) => {
+    const wordContainer = document.getElementById("word-container");
+    wordContainer.innerHTML="";
+    for (let word of words) {
+        const div = document.createElement("div");
+        div.innerHTML = ` <p>${word.word}</p>
+        <p>${word.meaning}</p>`;
+        wordContainer.appendChild(div);
+    }
+}
+
+
+
+
+
 const displayLessons = lessons => {
     // 1. get the container and empty it
     const lessonsContainer = document.getElementById("lessons-container");
@@ -17,7 +40,7 @@ const displayLessons = lessons => {
 
         // 3. create element
         const btnDiv = document.createElement("div");
-        btnDiv.innerHTML = `  <button class="btn btn-outline btn-primary ">
+        btnDiv.innerHTML = `  <button onclick="loadLevelWords(${lesson.level_no})" class="btn btn-outline btn-primary ">
               <i class="fa-solid fa-book-open"></i> Lesson-${lesson.level_no} </button> `;
 
         // 4. append to container
